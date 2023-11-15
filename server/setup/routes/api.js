1c0bebd5605f16eac4451924884f05ebcc3ba419
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2023-11-15 10:12:03 +0100

    create backend server

---
+++ server/setup/routes/api.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const data_db = require('./.data_db.js');
+
+const { Sequelize, DataTypes } = require("sequelize");
+const { status } = require("express/lib/response");
+
+const bcrypt = require('bcrypt');
+
+const sequelize = new Sequelize(
+  data_db.database,
+  data_db.username,
+  data_db.password,
+  {
+    dialect: "mysql",
+    host: "localhost",
+    port: data_db.port,
+  }
+);
+
+try {
+  console.log("Trying to connect to MySql database...");
+  sequelize.authenticate();
+  console.log("Connected to MySql database!");
+} catch (error) {
+  console.error("Unable to connect", error);
+}
+
+router.use((req, res, next) => {
+  next();
+});
