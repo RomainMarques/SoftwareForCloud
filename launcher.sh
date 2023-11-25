4ce7df01a6302f46916f8cd99b901fd4fe4090f8
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2023-11-25 18:50:09 +0100

    adding deployment and services for server + bdd

--- launcher.sh
+++ launcher.sh
@@ -1,2 +1,19 @@
 # Step 1 : Launch DockerCompose file
 docker compose up -d
+
+# Step 2 : Tag the images created and then push it, ex :
+#docker tag 336e8f7ed40e azarel9/softwareforcloud-server:0.1
+#docker tag azarel9/softwareforcloud-mysql:0.1
+
+# Step 3 : Push the images, ex :
+#docker push azarel9/softwareforcloud-mysql:0.1
+
+# Step 4, se placer dans le dossier kubernetes :
+cd ./kubernetes
+#kubectl apply -f database_deployment.yaml
+#kubectl apply -f database_service.yaml
+#kubectl apply -f server_deployment.yaml
+#kubectl apply -f server_service.yaml
+#minikube service mysql-deployment --url
+# Pour se connecter au serveur pour l'instant :
+#minikube service server --url
