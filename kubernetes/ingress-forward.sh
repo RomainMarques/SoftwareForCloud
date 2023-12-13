cbd10c0584f09672564c0f4bc0c1b9deeda02744
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2023-12-13 11:03:00 +0100

    service mesh for server

--- kubernetes/ingress-forward.sh
+++ kubernetes/ingress-forward.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo Open Ingress at http://localhost:31380/
-kubectl -n istio-system port-forward deployment/istio-ingressgateway 31380:8100
+echo Open Ingress at http://localhost:31380/server/getuser/c@c
+kubectl -n istio-system port-forward deployment/istio-ingressgateway 31380:8080
